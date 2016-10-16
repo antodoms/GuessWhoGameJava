358cc034e382e461cd74cff276f1f2baf75c6f33
Akshay Chawla <s3533550@student.rmit.edu.au>
2016-10-16 17:15:39 +1100

    Update BestAttribute.java
    commit " update commenting"

--- src/BestAttribute.java
+++ src/BestAttribute.java
@@ -1,12 +1,12 @@
 
 public class BestAttribute {
-	public String name;
-	public String value;
-	public int count;
+	public String name; // specifies the name of the attribute
+	public String value;  // specifies the attribute value
+	public int count; // specifies no of players who has the same <attribute, value>
 	
 	public BestAttribute(String name,String value,int count){
 		this.name = name;
 		this.value = value;
 		this.count = count;
 	}
-}
+} // End of Best Attribute Class
